f249c724ad0a31390b74064bdb00ec23fe447411
Mr Lightspeed <95941244+MrLightspeed@users.noreply.github.com>
2025-06-16 00:39:04 -0400

    chore: format docs and scripts

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,7 +1,7 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-import '/public/lighthouse.js'
+import "/public/lighthouse.js"
 
 $w.onReady(function () {
   // Write your JavaScript here
